fab475e8b7ef9c10bd2bc0b4cb54b5c890bfa015
Valentin271 <valentin.d77@free.fr>
2024-01-18 14:17:21 +0100

    now using alacritty as main terminal

--- i3/new-shell.sh
+++ i3/new-shell.sh
@@ -4,7 +4,13 @@ if [[ -f /tmp/cwd ]]; then
     # Needs `export PROMPT_COMMAND="pwd > /tmp/cwd"`
     WD=$(cat /tmp/cwd)
 else
-    WD=$(xprop -id $(xdotool getactivewindow) WM_NAME | grep -Po ': \K([^"]+)')
+    WD=$HOME
 fi
 
-xfce4-terminal --working-directory="${WD/#\~/$HOME}"
+if command -v alacritty > /dev/null; then
+    alacritty --working-directory="${WD/#\~/$HOME}"
+elif command -v xfce4-terminal > /dev/null; then
+    xfce4-terminal --working-directory="${WD/#\~/$HOME}"
+else
+    gnome-terminal --working-directory="${WD/#\~/$HOME}"
+fi
